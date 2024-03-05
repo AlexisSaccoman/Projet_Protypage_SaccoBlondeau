d9aabea981061716ef9543c05ebe2a8b45c5f9a2
AlexisSaccoman <alexis.saccoman@alumni.univ-avignon.fr>
2024-03-05 15:01:03 +0100

    update config

---
+++ src/main/java/controllers/divers/Personne.java
@@ -0,0 +1,11 @@
+package controllers.divers;
+
+public class Personne {
+    String statut;
+    String login;
+
+    public Personne(String statut, String login) {
+        this.statut = statut;
+        this.login = login;
+    }
+}
