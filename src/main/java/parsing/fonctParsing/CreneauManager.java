7206ef56f1cb7aec679944e7afa659f0bffd561f
Pierre <niono235@gmail.com>
2024-03-20 15:01:17 +0100

    Manager de créneau + setter getter

---
+++ src/main/java/parsing/fonctParsing/CreneauManager.java
@@ -0,0 +1,16 @@
+package parsing.fonctParsing;
+
+import java.util.ArrayList;
+
+public class CreneauManager {
+
+    ArrayList<Creneau> cours = new ArrayList<>();
+
+    public ArrayList<Creneau> getCours() {
+        return cours;
+    }
+
+    public void setCours(ArrayList<Creneau> cours) {
+        this.cours = cours;
+    }
+}
